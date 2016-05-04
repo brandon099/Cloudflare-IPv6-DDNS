86bd36490d9cc8991530fa1fa35a32376f6d9cbe
heywoodlh <l.spencer.heywood@gmail.com>
2016-05-03 18:13:57 -0600

    Create cf-info.sh
    Script cf-info.sh created to give user the Cloudflare DNS info needed for cf-ddns.sh to work properly.

---
+++ cf-info.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Script that will produce JSON output to help user obtain information needed for cf-ddns.sh
+
+# 1. Set "tkn" to equal your API key.
+# 2. Set "email" to equal your e-mail address
+# 3. Set "z" to equal the domain name that the zone record is associated to
+
+curl https://www.cloudflare.com/api_json.html \
+  -d 'a=rec_load_all' \
+  -d 'tkn=9a7806061c88ada191ed06f989cc3dac' \
+  -d 'email=email@example.com' \
+  -d 'z=example.com'

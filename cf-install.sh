5e5cabf5a6e04a590b80e5e24790bf8257320f86
Brandon Clifford <brandon.clifford@vivint.com>
2016-07-17 20:46:06 -0600

    Update cf-install
    '#!/usr/bin/env sh' is better than '#!/bin/sh'
    Use $() subshell syntax over ``
    Use Dependency array to easily add or remove dependencies in for loop
    check
    Consistent "if []; then" syntax
    Consistent Comment syntax if you are adding documentation comments
    Upper-case variables to upper case and be more descriptive with their
    names
    Use -w flag to check if the /var/tmp/current_ip.txt is writable
    Remove unneeded sleeps
    Format output messages (different levels such as info, warning, and
    error)
    Use <<< EOF, EOF > file.sh syntax instead of echo
    Allow user to define location of current_ip.txt (make it a variable)

--- cf-install.sh
+++ cf-install.sh
@@ -1,114 +1,93 @@
-#!/bin/bash
-
+#!/usr/bin/env sh
 ###Installer for cf-ddns.sh###
 
-echo "This script will help you configure a working Cloudflare DDNS script on a Linux machine. Please ensure that the programs 'jq' and 'grep' are installed in order for the installer to work"
-echo -e ' \t '
+# Output formatting
+info() { echo -e "\e[34m$@\e[0m"; }
+warning() { echo -e "\e[33m$@\e[0m"; }
+error() { echo -e "\e[31m$@\e[0m"; }
 
-sleep 2
+# Do not want to run as root user
+test "$UID" -gt 0 || { error "This script cannot be run as root."; exit;}
 
-echo "Please copy and paste your Cloudflare API Key now (found at https://www.cloudflare.com/a/account/my-account > Global API Key)"
-read API
+# Check if dependencies are met before running
+DEPS=(jq grep)
+for i in $DEPS; do
+  command -v $i >/dev/null 2>&1 || { error >&2 "Please install $i first"; exit 1; }
+done
 
-echo -e ' \t '
+info "This script will help you configure a working Cloudflare DDNS script for a Linux machine.\n"
+info "Please enter your Cloudflare API Key:"
+read API_KEY
 
-echo "Please enter your Cloudflare user e-mail address."
-read email
+info "Please enter your Cloudflare user e-mail address:"
+read EMAIL
 
-echo -e ' \t '
+info "Please enter the domain name associated with your Cloudflare account:"
+read DOMAIN
 
-echo "Please enter the domain name associated with your Cloudflare account"
-read domain
+info "Loading Cloudflare information"
+curl https://www.cloudflare.com/api_json.html \
+  -d'a=rec_load_all'\
+  -d tkn=$API_KEY \
+  -d =$EMAIL \
+  -d z=$DOMAIN \
+  | jq '.'| grep -B 5 '"type": "A".'
 
-echo -e ' \t '
+info "Locate the rec_id and zone name of the A record you would like to assign to the DDNS script."
+read -n1 -r -p "Press space to continue..." SPACE_KEY
 
+if [ "$SPACE_KEY" = '' ]; then
+    info "Now we will configure the ddns script."
+    info "Please enter your rec ID:"
+    read REC_ID
 
-
-echo "Loading Cloudflare information...."
-sleep 3
-echo -e ' \t '
-echo -e ' \t '
-echo -e ' \t '
-
-{ curl https://www.cloudflare.com/api_json.html \
-  -d 'a=rec_load_all' \
-  -d tkn=$API \
-  -d email=$email \
-  -d z=$domain
-} > json.txt
-
-cat json.txt | jq '.' | grep -B 5 '"type": "A".'
-
-sleep 2
-
-echo -e ' \t '
-echo -e ' \t '
-echo -e ' \t '
-echo "Locate the rec_id and zone name of the A record you would like to assign to the DDNS script"
-sleep 2
-echo -e ' \t '
-echo -e ' \t '
-read -n1 -r -p "Press space to continue..." key
-
-if [ "$key" = '' ]; then
-    echo "Now we will configure the ddns script." 
-    sleep 3
-    echo "Please enter your rec ID"
-
-    read id
-    
-    echo -e ' \t '
-    echo "Please enter the zone name of your A record (i.e. "server" in server.example.com)"
-    read name
-
-
- 
+    info "Please enter the zone name of your A record (i.e. 'server' in server.example.com)"
+    read $ZONE_NAME
 else
-    echo "Goodbye"
+    info "Exiting script"
     exit
 fi
 
-echo '#!/bin/sh
+# Generate cf-ddns.sh script in the current directory
+info "Generating cf-ddns.sh"
 
-if [ ! -f /var/tmp/current_ip.txt ] && touch /var/tmp/currentip.txt; then 
-echo "Running script"
-sleep 2
+cat << EOF > ./cf-ddns.sh
+#!/usr/bin/env sh
 
-else 
-echo
-"Error!"
-echo -e ' \t '
+CURR_IP_FILE=/var/tmp/current_ip.txt
 
-"If you received a permission denied error that means permissions for /var/tmp/current_ip.txt are not correct for user executing cf-ddns.sh. Please change the ownership of /var/tmp/current_ip.txt to the user running the DDNS script. If you received a different error please reach out to the author at http://www.primal.support/contact."
-exit 1
-fi 
+# Try to write to the current IP address file, and exit if it cannot
+( [ -e "\$CURR_IP_FILE" ] || touch "\$CURR_IP_FILE" ) && [ ! -w "\$CURR_IP_FILE" ] && error "Cannot write to $CURR_IP_FILE" && exit 1
 
-##Set variable NEWIP to the public IPv4 address of the network that your server is currently connected to##
-NEWIP=`dig +short myip.opendns.com @resolver1.opendns.com`
+# Set NEWIP to the public IPv4 address of the network that your server is currently connected to
+NEWIP=\$(dig +short myip.opendns.com @resolver1.opendns.com)
 
+# Set CURRENTIP to whatever IPv4 address your domain name is currently pointed to
+CURRENTIP=\$(getent ahosts $DOMAIN | awk '{print \$1}' | head -1)
 
-##Set variable CURRENTIP to whatever IPv4 address your domain name is currently pointed to##
-CURRENTIP=`getent ahosts $DOMAIN | awk '{print $1}' | head -1`
-
-
-if [ "$NEWIP" = "$CURRENTIP" ]
-then
+# Compare IP addresses to see if anything has changed, and update CloudFlare if it has
+if [ "\$NEWIP" = "\$CURRENTIP" ]; then
   echo "IP address unchanged"
 else
+  echo "IP address has changed, updating CloudFlare..."
+
   curl https://www.cloudflare.com/api_json.html \
-    -d 'a=rec_edit' \
-    -d 'tkn=$API' \
-    -d 'email=$email' \
-    -d 'z=$domain' \
-    -d 'id=$id' \
-    -d 'type=A' \
-    -d 'name=$name' \
-    -d 'ttl=1' \
-    -d "content=$NEWIP"
-  echo $NEWIP > /var/tmp/currentip.txt
-fi' > cf-ddns.sh
+    -d 'a=rec_edit'\
+    -d 'tkn=$API_KEY'\
+    -d '=$EMAIL'\
+    -d 'z=$DOMAIN'\
+    -d 'id=$REC_ID'\
+    -d 'type=A'\
+    -d 'name=$ZONE_NAME'\
+    -d 'ttl=1'\
+    -d "content=\$NEWIP"
 
+  # Update the tracked IP address
+  echo \$NEWIP > \$CURR_IP_FILE
+fi
+EOF
 
+info "Adding executable bit on cf-ddns.sh script"
 chmod u+x cf-*
 
-echo "Within your directory will now be a functioning cf-ddns.sh script. Execute it by running the following command: ./cf-ddns.sh"
+info "Within this directory will now be a fully configured cf-ddns.sh script. Execute it by running the following command: ./cf-ddns.sh"
